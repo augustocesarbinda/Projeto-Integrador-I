0f91e9adf9feb88ca616156cedcdb318c74f1d1c
augustocesarbinda <2002378@aluno.univesp.br>
2021-11-19 01:03:27 -0300

    incluida barra de navegação

--- index.php
+++ index.php
@@ -16,15 +16,26 @@
             </div>
         </header>
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand" href="#">
                 <img src="img/coruja_peq.png">
             </a>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Quem somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Instruções</a>
+                    </li>
+                </ul>
         </nav>
 
         <main>
-            <button type="button" class="btn btn-primary">Primary</button>
-
+           
         </main>
 
 
