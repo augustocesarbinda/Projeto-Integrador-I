556d2bf42fc9974fd894b5b881dd942902168ff6
augustocesarbinda <2002378@aluno.univesp.br>
2021-11-19 16:37:11 -0300

    ajustes layout e menu

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <title>Plano de Aula</title>
@@ -10,25 +11,32 @@
     
     <body>
         <header>
-                <h2 class="logo">PLANO DE AULA</h2>
+            <div class="navbar container-fluid">
+                <div class="container">
+                    <h2 class="logo">PLANO DE AULA</h2>
+                </div>
+            </div>
         </header>
 
         <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="#">
-                <img src="img/coruja_peq.png">
-            </a>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Quem somos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Instruções</a>
-                    </li>
-                </ul>
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="img/coruja_peq.png">
+                </a>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sobre nós</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Instruções</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
         
 
