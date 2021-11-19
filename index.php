fe5b14cf2521e455d6e94c92ed3e6301c5377f03
augustocesarbinda <2002378@aluno.univesp.br>
2021-11-19 01:35:16 -0300

    alteração background

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE-edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <title>Plano de Aula</title>
     </head>
     
